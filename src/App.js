df23d8e9ae478d207addf2f2c3b88a3e27a9c2b9
harkiratsm <multaniharry714@gmail.com>
2021-11-13 19:34:59 +0530

    ✅ Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar'
 import './App.css'
 import Main from './components/Main'
 import Group from './components/Group'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="app-header">
       <Navbar />
       <Group />
       <Main />
+      <Footer />
     </div>
   )
 }
