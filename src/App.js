5b254af29b533423378ac85651aaedfa46f1f1dc
harkiratsm <multaniharry714@gmail.com>
2021-11-13 19:17:20 +0530

    ✅ Main Page , button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import './App.css'
+import Main from './components/Main'
+import Group from './components/Group'
 function App() {
   return (
-    <div>
+    <div className="app-header">
       <Navbar />
+      <Group />
+      <Main />
     </div>
   )
 }
